6f63568a1e579b495d79e9480babf2a9c4dcfdbc
Steve-Munoz <52468605+Steve-Munoz@users.noreply.github.com>
2023-11-13 03:20:22 +0000

    created movie search App

---
+++ movie-search-app/src/app/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {createRoot}from "react-dom/client"
+import {BrowserRouter as Router} from "react-router-dom"
+//import {ContextProvider} from "./Context"
+import App from "./App"
+
+const container = document.getElementById('root');
+const root = createRoot(container)
+root.render(
+
+  <Router>
+    <App />
+  </Router>
+)
+
